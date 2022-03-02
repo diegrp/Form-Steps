3bc532cb2f989b91eb29dbb9d677fb07a7442120
Diego Rocha <dkxever@gmail.com>
2022-03-01 22:53:20 -0300

    Structuring and organizing our Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,12 @@
+import * as C from "./styles";
+
+// FormSteps: Header
+
 export const Header = () => {
   return(
-    <div>
-      ...
-    </div>
+    <C.Container>
+      <h1>Cadastro do Desenvolvedor</h1>
+      <p>Preencha os campos na lista, e receba um emprego imediato!</p>
+    </C.Container>
   )
 }
