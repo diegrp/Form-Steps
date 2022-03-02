8706d0c7b72771473aa78080b4eb1c3ed3d443bd
Diego Rocha <dkxever@gmail.com>
2022-03-01 23:24:03 -0300

    Structuring and organizing our SidebarItem

--- src/Components/SidebarItem/index.tsx
+++ src/Components/SidebarItem/index.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import { ReactComponent as ProfileIcon } from "../../Svgs/profile.svg";
+import { ReactComponent as BookIcon } from "../../Svgs/book.svg";
+import { ReactComponent as MailIcon } from "../../Svgs/mail.svg";
 import * as C from "./styles";
 
 type Props = {
@@ -12,8 +16,25 @@ type Props = {
 
 export const SidebarItem = ( { title, description, icon, path, active }: Props ) => {
   return(
-    <div>
-      ...
-    </div>
+    <C.Container>
+      <Link to={path}>
+        <C.Info>
+          <C.Title>{title}</C.Title>
+          <C.Description>{description}</C.Description>
+        </C.Info>
+        <C.IconArea active={active}>
+          {icon === 'profile' && 
+            <ProfileIcon fill="white" width={24} height={24} />
+          }
+          {icon === 'book' && 
+            <BookIcon fill="white" width={24} height={24} />
+          }
+          {icon === 'mail' && 
+            <MailIcon fill="white" width={24} height={24} />
+          }
+        </C.IconArea>
+        <C.Point active={active}></C.Point>
+      </Link>
+    </C.Container>
   )
 }
