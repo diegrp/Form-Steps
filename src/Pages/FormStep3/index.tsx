bf68dc330b2ea3d39ff85496b2f34e8bd8f35c74
Diego Rocha <dkxever@gmail.com>
2022-03-01 23:13:14 -0300

    Structuring and organizing our FormStep3

--- src/Pages/FormStep3/index.tsx
+++ src/Pages/FormStep3/index.tsx
@@ -1,7 +1,53 @@
+import { Head } from "../../Routers/Head";
+import { Theme } from "../../Components/Theme";
+import { InputArea } from "../../Components/InputArea";
+import * as C from "./styles";
+
+// FormStep3: Terceira etapa do formulário (E-mail e Github)
+
 export const FormStep3 = () => {
+
   return(
-    <div>
-      ...
-    </div>
+    <Theme>
+      <C.Container>
+
+        {/* Atualiza o header e decription da página, de acordo com passo atual */}
+        <Head title="Contato" description="Como entrar em contato com você?" />
+        
+        <p>Step 3/4</p>
+        <h1>Então {state.name}, como podemos te achar?</h1>
+        <p>Preencha os campos abaixo, para entrarmos em contato com você</p>
+
+        <hr/>
+
+        <InputArea 
+          label="Qual o seu e-mail?" 
+          name="email"
+          id="email" 
+          type="email"
+          value={state.email}
+          onChange={handleEmailChange}
+          onBlur={handleEmailChange}
+          placeholder="Seu e-mail"
+          autoComplete="off"
+        />
+
+        <InputArea 
+          label="Qual o seu github?" 
+          name="github"
+          id="github" 
+          type="url"
+          value={state.github}
+          onChange={handleGithubChange}
+          onBlur={handleGithubChange}
+          placeholder="Seu github"
+          autoComplete="off"
+        />
+
+        <Link to="/step2" className="backButton">Voltar</Link>
+        <button onClick={handleNextStep}>Finalizar</button>
+
+      </C.Container>
+    </Theme>
   )
 }
