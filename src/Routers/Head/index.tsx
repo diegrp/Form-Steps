04060edeb58b63d3d5487b4502aee11cefa6c10f
Diego Rocha <dkxever@gmail.com>
2022-03-01 22:44:40 -0300

    Page head and description

--- src/Routers/Head/index.tsx
+++ src/Routers/Head/index.tsx
@@ -1,7 +1,20 @@
-export const Head = () => {
-  return(
-    <div>
-      ...
-    </div>
-  )
+import { useEffect } from "react";
+
+type Props = {
+  title: string
+  description: string
+}
+
+// FormSteps: Header e Description 
+
+export const Header = ( { title, description }: Props ): null => {
+  
+  useEffect(() => {
+    document.title = `Form Steps | ${title}` || "";
+    const desc =  document.querySelector("meta[name='description']");
+    desc?.setAttribute("content", description || "");
+  },[title, description]);
+
+  return null;
+  
 }
