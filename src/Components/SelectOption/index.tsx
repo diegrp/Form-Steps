f0d8cc6368f6e84a26fb76bbe64a3bc6f0624e5b
Diego Rocha <dkxever@gmail.com>
2022-03-01 23:09:57 -0300

    Passing and arranging the props for our SelectOption

--- src/Components/SelectOption/index.tsx
+++ src/Components/SelectOption/index.tsx
@@ -1,7 +1,23 @@
-export const SelectOption = () => {
+import * as C from "./styles";
+
+type Props = {
+  title: string
+  description: string
+  icon: string
+  selected: boolean
+  onClick: () => void
+}
+
+// Step 2 - Seleção de opção / level profissional
+
+export const SelectOption = ( { title, description, icon, selected, onClick }: Props ) => {
   return(
-    <div>
-      ...
-    </div>
+    <C.Container onClick={onClick} selected={selected}>
+      <C.Icon>{icon}</C.Icon>
+      <C.Info>
+        <C.Title>{title}</C.Title>
+        <C.Description>{description}</C.Description>
+      </C.Info>
+    </C.Container>
   )
 }
