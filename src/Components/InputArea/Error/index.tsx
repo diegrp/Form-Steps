2b1bbb700960703caefa82da35b07bd7df756593
Diego Rocha <dkxever@gmail.com>
2022-03-01 22:55:44 -0300

    Structuring and organizing our Error

--- src/Components/InputArea/Error/index.tsx
+++ src/Components/InputArea/Error/index.tsx
@@ -1,7 +1,44 @@
-export const Error = () => {
+import { ErrorType } from "../../../Contexts/FormContext";
+import * as C from "./styles";
+
+type Props = {
+  error: ErrorType
+}
+
+// Erro de validação: (name, email, github)
+
+export const Error = ( { error }: Props ) => {
   return(
-    <div>
-      ...
-    </div>
+    <C.Container>
+      {error.name && 
+        <C.Error>
+          <label>
+            {error.name}
+          </label>
+        </C.Error>
+      }
+      {error.email && 
+        <C.Error>
+          <label>
+            {error.email}
+          </label>
+        </C.Error>
+      }
+      {error.github && 
+        <C.Error>
+          <label>
+            {error.github}
+          </label>
+        </C.Error>
+      }
+
+      {error.empty &&
+        <C.Error>
+          <label>
+            {error.empty}
+          </label>
+        </C.Error>
+      }
+    </C.Container>
   )
 }
