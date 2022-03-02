abc40b99862c020ddd23fa95f45fcae026484ab1
Diego Rocha <dkxever@gmail.com>
2022-03-01 23:23:06 -0300

    Passing and arranging the props for our SidebarItem

--- src/Components/SidebarItem/index.tsx
+++ src/Components/SidebarItem/index.tsx
@@ -1,4 +1,16 @@
-export const SidebarItem = () => {
+import * as C from "./styles";
+
+type Props = {
+  title: string
+  description: string
+  icon: string
+  path: string
+  active: boolean
+}
+
+// Seleção da etapa do formulário pela sidebar
+
+export const SidebarItem = ( { title, description, icon, path, active }: Props ) => {
   return(
     <div>
       ...
