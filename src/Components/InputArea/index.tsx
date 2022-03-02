37bdfc7117a46e242d1e6a49dc8dd6b015d1c493
Diego Rocha <dkxever@gmail.com>
2022-03-01 22:54:31 -0300

    Structuring and organizing our InputArea

--- src/Components/InputArea/index.tsx
+++ src/Components/InputArea/index.tsx
@@ -1,7 +1,37 @@
-export const InputArea = () => {
+import { ChangeEvent } from "react";
+import * as C from "./styles";
+
+type Props = {
+  label: string
+  id: string
+  name: string
+  type: string
+  value: number | string
+  onChange: ( e: ChangeEvent <HTMLInputElement> ) => void
+  onBlur: ( e: ChangeEvent <HTMLInputElement> ) => void
+  placeholder: string
+  [x:string]: any
+}
+
+// Input element do formulário
+
+export const InputArea = ( { label, id, name, type, value, onChange, onBlur, placeholder, ...rest }: Props ) => {
+  
   return(
-    <div>
-      ...
-    </div>
+    <C.InputWrapper>
+      <label htmlFor={name}>
+        {label}
+        <input
+          id={name}
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onChange}
+          placeholder={placeholder}
+          {...rest}
+        />
+      </label>
+    </C.InputWrapper>
   )
 }
