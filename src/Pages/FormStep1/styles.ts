663fff3d80a3721e756a0f80f435bc83e143b4a0
Diego Rocha <dkxever@gmail.com>
2022-03-01 23:04:30 -0300

    Styling our FormStep1 component

---
+++ src/Pages/FormStep1/styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  p{
+    font-size: var(--size-small);
+    color: var(--color-p);
+    margin-bottom: 7px;
+  }
+
+  h1{
+    font-size: var(--size-large);
+    font-weight: bold;
+    margin-bottom: 7px;
+    color: #fff;
+  }
+
+  hr{
+    height: 1px;
+    border: none;
+    background-color: var(--background-hr);
+    margin: 20px 0;
+  }
+
+  label{
+    display: block;
+    font-size: var(--size-small);
+    color: #fff;
+    margin-bottom: 15px;
+  }
+
+  button{
+    font-size: var(--size-medium);
+    color:#fff;
+    background-color: var(--background-sidebar-bt);
+    font-weight: bold;
+    padding: 20px 40px;
+    border: none;
+    border-radius: 40px;
+    margin: 10px 0;
+    transition: 0.3s linear;
+    cursor: pointer;
+  
+    &:hover{
+      background-color: var(--background-emoji-bthover);
+    }
+  
+  }
+`;
